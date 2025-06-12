4a8b18092c748f16ac6d0552f83dce91a14c0ba7
Pbourgeois62 <pbourgeois62200@gmail.com>
2025-06-12 11:50:57 +0200

    DONE - a user can register, connect, and access to the homepage.

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'slide-in': {
+          '0%': {
+            transform: 'translateX(-100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 0.5s ease-out forwards',
+      },
+    },
+  },
+  plugins: [],
+}
