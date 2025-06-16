5e39855e7ea3b3df04abf69da9ab397000147d01
Pbourgeois62 <pbourgeois62200@gmail.com>
2025-06-16 19:37:07 +0200

    DONE- Livecomponent + filtered view

---
+++ src/Twig/Components/Form/filterSaleEventsByStatus.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Twig\Components\Form;
+
+use App\Form\SaleEventStatusForm;
+use App\Repository\SaleEventRepository;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[AsLiveComponent]
+class filterSaleEventsByStatus extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    public array $saleEvents = [];
+
+    private SaleEventRepository $saleEventRepository;
+
+    public function __construct(SaleEventRepository $saleEventRepository)
+    {
+        $this->saleEventRepository = $saleEventRepository;
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(SaleEventStatusForm::class);
+    }
+
+    #[LiveAction]
+    public function filterByStatus(): void
+    {
+        $this->submitForm();
+        $status = $this->getForm()->get('status')->getData();
+        switch ($status) {
+            case 'passed':
+                $this->saleEvents = $this->saleEventRepository->findPassedSaleEvents();
+                break;
+            case 'today':
+                $this->saleEvents = $this->saleEventRepository->findTodaySaleEvents();
+                break;
+            case 'incoming':
+                $this->saleEvents = $this->saleEventRepository->findIncomingSaleEvents();
+                break;
+            default:
+                $this->saleEvents = $this->saleEventRepository->findIncomingSaleEvents();
+                break;
+        }
+    }    
+}
